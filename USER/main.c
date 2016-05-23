9e0a5ec3b8ef62db139a530f6db3c2ea9bb280b3
luhuadong <luhuadong@163.com>
2016-05-23 15:34:38 +0800

    Initial submission
    This is a template of STM32 in MDK-ARM.

---
+++ USER/main.c
@@ -0,0 +1,38 @@
+/**
+  ******************************************************************************
+  * @file    STM32-Template/USER/main.c 
+  * @author  Lu HuaDong
+  * @version V3.5.0
+  * @date    23-May-2016
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  * 
+  * The present firmware which is for guidance only aims at providing customers
+  * with coding information regarding their products in order for them to save
+  * time. As a result, STMicroelectronics shall not be held liable for any
+  * direct, indirect or consequential damages with respect to any claims arising
+  * from the content of such firmware and/or the use made by customers of the
+  * coding information contained herein in connection with their products.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */  
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include <stdio.h>
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  while (1)
+  {
+  }
+}
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
